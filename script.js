edd10cdb6073c16dd02ed5eda6420fa1e11e09a0
SzilviaFici <sziszfici@gmail.com>
2022-06-23 11:33:12 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+class autos{
+    constructor(Modell,Farbe,PS){
+        this.Modell = Modell
+        this.Farbe = Farbe
+        this.PS = Ps
+    }
+
+
+}
+function schnellfahren
+Fahrzeug
---
+++ scriptbuch.js
@@ -0,0 +1,36 @@
+class Buch{
+    BuchVerfuegung = false
+    BuchVerkauft = false
+    Genre
+    Erscheinungsjahr
+    Autor
+    Preis
+    Exemplar
+
+
+    constructor(Genre,Erscheinungsjahr, Autor, Preis,Anzahl){
+        this.Genre = Genre
+        this.Erscheinungsjahr = Erscheinungsjahr
+        this.Autor = Autor
+        this.Preis = Preis
+        this.Exemplar = Anzahl
+    }
+
+
+    verfuegbar() {
+    if (this.Exemplar>0) {
+        console.log("Buch steht zu Verfügung")
+    }else{
+        console.log("Buch steht nicht zu Verfügung")
+    }
+    }
+
+    verkauft() {
+        if (this.Exemplar-1) {
+            console.log("Buch wurde verkauft")
+    }else{
+    console.log("Das Buch wurde nicht verkauft")
+    }
+}
+}
+
